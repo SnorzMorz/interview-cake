2d247765e90e97721887d18243bd02278373f63f
SnorzMorz <68282318+SnorzMorz@users.noreply.github.com>
2022-03-16 18:00:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -79,3 +79,75 @@ def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
     if (take_out_num < len(take_out_orders) or dine_in_num < len(dine_in_orders)):
         return False
     return True
+
+
+def contains_cycle(first_node):
+    visited = set()
+    while first_node is not None:
+
+        if (first_node in visited):
+            return True
+        else:
+            visited.add(first_node)
+
+        first_node = first_node.next
+
+    return False
+
+
+class Stack(object):
+
+    def __init__(self):
+        """Initialize an empty stack"""
+        self.items = []
+
+    def push(self, item):
+        """Push a new item onto the stack"""
+        self.items.append(item)
+
+    def pop(self):
+        """Remove and return the last item"""
+        # If the stack is empty, return None
+        # (it would also be reasonable to throw an exception)
+        if not self.items:
+            return None
+
+        return self.items.pop()
+
+    def peek(self):
+        """Return the last item without removing it"""
+        if not self.items:
+            return None
+        return self.items[-1]
+
+
+class MaxStack(object):
+
+    def __init__(self):
+        self.stack = Stack()
+        self.maxes_stack = Stack()
+
+    def push(self, item):
+        """Add a new item onto the top of our stack."""
+        self.stack.push(item)
+
+        # If the item is greater than or equal to the last item in maxes_stack,
+        # it's the new max! So we'll add it to maxes_stack.
+        if self.maxes_stack.peek() is None or item >= self.maxes_stack.peek():
+            self.maxes_stack.push(item)
+
+    def pop(self):
+        """Remove and return the top item from our stack."""
+        item = self.stack.pop()
+
+        # If it equals the top item in maxes_stack, they must have been pushed
+        # in together. So we'll pop it out of maxes_stack too.
+        if item == self.maxes_stack.peek():
+            self.maxes_stack.pop()
+
+        return item
+
+    def get_max(self):
+        """The last item in maxes_stack is the max item in our stack."""
+        return self.maxes_stack.peek()
+
