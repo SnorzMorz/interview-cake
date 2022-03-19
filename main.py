0589d9336060f6b177d057b4fffa0c39c7464e2f
SnorzMorz <68282318+SnorzMorz@users.noreply.github.com>
2022-03-19 17:06:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -151,3 +151,17 @@ class MaxStack(object):
         """The last item in maxes_stack is the max item in our stack."""
         return self.maxes_stack.peek()
 
+
+def can_two_movies_fill_flight(movie_lengths, flight_length):
+    # Movie lengths we've seen so far
+    movie_lengths_seen = set()
+
+    for first_movie_length in movie_lengths:
+        matching_second_movie_length = flight_length - first_movie_length
+        if matching_second_movie_length in movie_lengths_seen:
+            return True
+        movie_lengths_seen.add(first_movie_length)
+
+    # We never found a match, so return False
+    return False
+
